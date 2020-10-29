230f483ebba15ac633aa3f5615a6e352a513e630
CarlsDEV <youquince@gmail.com>
2020-10-29 17:59:42 -0500

    Referencias en NodeJS con Populate

--- src/controllers/apiFlutter.controllers.js
+++ src/controllers/apiFlutter.controllers.js
@@ -5,7 +5,14 @@ const User = require('../models/User');
 
 // Listar Notas API
 flutterControllers.renderNotesAPI = async (req, res) => {
+   /* ?Solo muestra todas las notas */
    const listNotasAPI = await Notas.find().lean();
+
+   /* ?Devuelve todo el campo del user completo */
+   // const listNotasAPI = await Notas.find().populate("userid").exec();
+
+   /* ?Devuelve determinados datos del user completo */
+   // const listNotasAPI = await Notas.find().populate("userid", "cedula apellidos nombres edad genero email").exec();
    res.json({ notes: listNotasAPI });
 }
 
